ebb5fc7071dfde5485b7ebfc0691e3b9e1031d0e
Austin Luper <austin.luper@gmail.com>
2023-12-17 23:23:18 -0500

    Removed unnecessary comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-//**CODE TO BE RELOCATED TO APPROPRIATE LOCATION**
-
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
