82d85bc48b4284f5a5a68ca4d7e090d30790cf10
Austin Luper <austin.luper@gmail.com>
2023-12-17 20:59:24 -0500

    Deleted commnets that are no longer relevant

--- Develop/script.js
+++ Develop/script.js
@@ -12,11 +12,6 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   // Get the current hour using dayjs, using 24-hour format
  var currentHour = dayjs().hour();
  
